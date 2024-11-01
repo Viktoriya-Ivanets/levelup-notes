ffd7b73252fb059b86de2c70e648d96e085fa044
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-01 13:45:29 +0200

    Controller.php - Create render function and action functions

---
+++ app/core/Controller.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once 'app/classes/Session.php';
+require_once 'app/controllers/AuthController.php';
+
+class Controller
+{
+    private AuthController $authController;
+
+    public function __construct()
+    {
+        $this->authController = new AuthController();
+    }
+
+    /**
+     * Renders the specified view.
+     *
+     * @param string $page The name of the view to render.
+     * @param array $data Data to pass to the view.
+     * @param string $template The name of the template to use.
+     * @return void
+     */
+    public function renderView(string $page, array $data = [], string $template = 'layout'): void
+    {
+        extract($data);
+        require "app/views/templates/{$template}.php";
+    }
+
+    /**
+     * Index action that determines if the user is authenticated.
+     *
+     * @return void
+     */
+    public function index(): void
+    {
+        Session::start();
+        if (Session::isAuthenticated()) {
+            $this->renderView('index', ['username' => Session::get('username')]);
+        } else {
+            $this->login();
+        }
+    }
+
+    /**
+     * Redirects to the login view.
+     *
+     * @return void
+     */
+    public function login(): void
+    {
+        $this->authController->login();
+    }
+
+    /**
+     * Redirects to the registration view.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->authController->register();
+    }
+
+    /**
+     * Redirects to the logout method.
+     *
+     * @return void
+     */
+    public function logout(): void
+    {
+        $this->authController->logout();
+    }
+}
