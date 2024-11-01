482fd6cee13964d1af51d45ac4d2e72dfc44edad
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-01 17:19:34 +0200

    index.php - Create template for notes

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,1 +1,28 @@
-<h1>Index page</h1>
+<div class="w3-container">
+    <h2>Add notes</h2>
+    <form method="POST" action="<?= Router::url('add') ?>">
+        <input class="w3-input" type="text" name="note" required placeholder="Текст нотатки">
+        <button class="w3-button w3-green w3-margin-top" type="submit">Add</button>
+    </form>
+
+    <h2>Notes list</h2>
+    <table class="w3-table w3-bordered">
+        <tr>
+            <th>№</th>
+            <th>Note's text</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($notes as $index => $note): ?>
+            <tr>
+                <td><?= $index + 1 ?></td>
+                <td><?= htmlspecialchars($note['text']) ?></td>
+                <td>
+                    <form method="POST" action="<?= Router::url('delete') ?>">
+                        <input type="hidden" name="index" value="<?= $index ?>">
+                        <button class="w3-button w3-red" type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
