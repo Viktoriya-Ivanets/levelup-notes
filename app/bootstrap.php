1dad1448acaa190c5bdc807df8fdf4391f21db08
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-01 14:01:53 +0200

    bootstrap.php - Refactore autoload function

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,17 +2,13 @@
 
 require_once 'app/config.php';
 
-spl_autoload_register(function ($className) {
-    $classFile = CLASSES_PATH . $className . '.php';
-    if (file_exists($classFile)) {
-        include_once $classFile;
-        return true;
-    }
-
-    $coreFile = CORE_PATH . $className . '.php';
-    if (file_exists($coreFile)) {
-        include_once $coreFile;
-        return true;
+spl_autoload_register(function ($className) use (CLASS_PATHS) {
+    foreach (CLASS_PATHS as $path) {
+        $classFile = $path . $className . '.php';
+        if (file_exists($classFile)) {
+            include_once $classFile;
+            return true;
+        }
     }
     return false;
 });
