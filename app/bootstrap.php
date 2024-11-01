ca1c7a3a7fb253c7aecf80bf4a6bfe94933f5114
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-11-01 17:13:24 +0200

    bootstrap.php - Fix issue with classes constant

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,8 +2,10 @@
 
 require_once 'app/config.php';
 
-spl_autoload_register(function ($className) use (CLASS_PATHS) {
-    foreach (CLASS_PATHS as $path) {
+$classPaths = CLASS_PATHS; // Створюємо змінну з константи
+
+spl_autoload_register(function ($className) use ($classPaths) {
+    foreach ($classPaths as $path) {
         $classFile = $path . $className . '.php';
         if (file_exists($classFile)) {
             include_once $classFile;
